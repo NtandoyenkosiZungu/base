6f60d2c37535133e4c85144bc4035e396a2810e6
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-04-29 03:53:56 +0200

    Think I am done working on template-four for now

--- src/components/Templates/TemplateFour.tsx
+++ src/components/Templates/TemplateFour.tsx
@@ -53,23 +53,23 @@ export const TemplateFour: React.FC = () => {
             <section className='template-four-body-personal-details'>
                 <div className='template-four-body-personal-details-left'>
                     <h3>Contact Details</h3>
-                    <p>
+                    <p className={address.length > 0 ? 'visible': 'none-display'}>
                         <img src={icons.location} alt="location" />
                         <span>{address}</span>
                     </p>
-                    <p>
+                    <p className={phone.length > 0 ? 'visible': 'none-display'}>
                         <img src={icons.phone} alt="phone" />
                         <a href={`tel:${phone}`} target='_blank' rel='noopener noreferrer' style={{color: '#2D2C2C', cursor: 'pointer'}}>{phone}</a>
                     </p>
-                    <p>
+                    <p className={email.length > 0 ? 'visible': 'none-display'}>
                         <img src={icons.email} alt="email" />
                         <a href={`mailto:${email}`} target='_blank' rel='noopener noreferrer' style={{color: '#2D2C2C', cursor: 'pointer'}}>{email}</a>
                     </p>
-                    <p>
+                    <p className={linkedin.length > 0 ? 'visible': 'none-display'}>
                         <img src={icons.linkedin} alt="linkedin" />
                         <a href={linkedin} target='_blank' rel='noopener noreferrer' style={{color: '#2D2C2C', cursor: 'pointer'}}>{linkedin}</a>
                     </p>
-                    <p>
+                    <p className={github.length > 0 ? 'visible': 'none-display'}>
                         <img src={icons.github} alt="github" />
                         <a href={github} target='_blank' rel='noopener noreferrer' style={{color: '#2D2C2C', cursor: 'pointer'}}>{github}</a>
                     </p>
@@ -81,81 +81,85 @@ export const TemplateFour: React.FC = () => {
                     </pre>
                 </div>
             </section>
+
             <section className='template-four-body-education'>
                 <div className='template-four-body-education-left'>
-                    <div className='holder'>
+                    <section className={educationEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Education</h3>
                         {
                             educationEntries.map((entry, index) => (
                                 <div key={index}>
                                     <h4>{entry.institution}</h4>
-                                    <p>
+                                    <p className='visible'>
                                         <i>{entry.start_date} - {entry.end_date}</i>
                                     </p>
-                                    <p>
+                                    <p className='visible'>
                                         <i>{entry.level} in {entry.field}</i>
                                     </p>
                                 </div>
                             ))
                         }
-                    </div>
-                    <div className='holder'>
+                    </section>
+
+                    <section className={CertificationEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Certifications</h3>
                         {
                             CertificationEntries.map((entry, index) => (
                                 <div key={index}>
                                     <h4>{entry.provider}</h4>
-                                    <p>
+                                    <p className='visible'>
                                         <i>{entry.title}</i>
                                     </p>
-                                    <p>
+                                    <p className='visible'>
                                         <i>{entry.date}</i>
                                     </p>
                                 </div>
                             ))
                         }
-                    </div>
-                    <div className='holder'>
+                    </section>
+
+                    <section className={TechnicalSkillEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Technical Skills</h3>
                         {
                            TechnicalSkillEntries.map((entry, index) => (
                             <div key={index} dangerouslySetInnerHTML={{__html: entry.skill}}></div>
                            ))
                         }
-                    </div>
-                    <div>
+                    </section>
+
+                    <section className={softSkillEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Soft Skills</h3>
                         {
                             softSkillEntries.map((entry, index) => (
                                 <div key={index} dangerouslySetInnerHTML={{__html: entry.skill}}></div>
                             ))
                         }
-                    </div>
+                    </section>
                 </div>
                 <div className='template-four-body-education-right'>
-                    <div>
+                    <section className={experienceEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Experience</h3>
                         {
                             experienceEntries.map((entry, index) => (
                                 <div key={index}>
-                                    <h4>{entry.workplace}, {entry.role}</h4>
-                                    <p>{entry.startDate} - {entry.endDate}</p>
+                                    <h4>{entry.workplace} | {entry.role}</h4>
+                                    <p className='visible'>{entry.startDate} - {entry.endDate}</p>
                                     <div dangerouslySetInnerHTML={{__html: entry.description}}></div>
                                 </div>
                             ))
                         }
-                    </div>
-                    <div className='holder'>
+                    </section>
+                    <section className={projectEntries.length > 0 ? 'holder': 'none-display'}>
                         <h3>Projects</h3>
                         {
                             projectEntries.map((entry, index) => (
                                 <div key={index}>
-                                    <p>{entry.project}</p>
-                                    <p>{entry.description}</p>
+                                    <h4 className='visible'>{entry.project} | <a href={entry.link} target='_blank' rel='noopener noreferrer' style={{color: '#2D2C2C', cursor: 'pointer'}}>LINK</a></h4>
+                                    <div dangerouslySetInnerHTML={{__html: entry.description}}></div>
                                 </div>
                             ))
                         }
-                    </div>
+                    </section>
                     
                 </div>
             </section>
