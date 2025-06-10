8f493aa2bf36b7ab2c0b558ab25c5ad814c8ec8e
Ntandoyenkosi Zungu <166859858+CelestialLelouch@users.noreply.github.com>
2025-06-11 00:31:15 +0200

    updated  styling to the sign up and login form

--- src/components/Home/LogIn.tsx
+++ src/components/Home/LogIn.tsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h2 className="login-title">Welcome to Jobify</h2>
+        <h2 className="login-title">Welcome to HiResume</h2>
         
         {/* Display login error */}
         {errorMessage && <p className="error-text">{errorMessage}</p>}
